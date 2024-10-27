4896c85fe9bd68ebc0fd629a167aae4e8a9a9a43
“jundalisay” <jundalisay@yahoo.com>
2024-10-27 17:57:53 +0800

    update

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -7,7 +7,7 @@
 	// import Pricing from './components/sections/pricing.svelte';
 	import Testimonials from './components/sections/testimonials.svelte';
 
-	export let data;
+	// export let data;
 
 	// const { prices } = data;
 </script>
